b0b0482545e704a2dd4bca0c18a122821c2eeee8
Joshua Della Vedova <jdellavedova3@gmail.com>
2026-04-26 20:54:24 -0700

    Remove sitemap integration (broken on Linux build runner)

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://jdellavedova.com",
@@ -8,5 +7,5 @@ export default defineConfig({
   build: {
     assets: "_assets",
   },
-  integrations: [sitemap()],
+  integrations: [],
 });
